9039aa613f5e4eb1bf1a5c5401bf2b16405cef50
Robert L <1998.robert@gmail.com>
2018-07-17 10:13:35 -0700

    Add text-alignment, undo and redo

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Editor, EditorState, RichUtils} from 'draft-js';
-
+import {Editor, EditorState, RichUtils, convertFromRaw} from 'draft-js';
 
 const styleMap = {
   'UPPERCASE': {
@@ -14,9 +13,24 @@ const styleMap = {
 
 export default class Documents extends React.Component {
 
+  myBlockStyleFn(contentBlock) {
+    const type = contentBlock.getType();
+    if (type === 'right') {
+      return 'align-right';
+    }
+    if (type === 'center') {
+      return 'align-center'
+    }
+    if (type === 'left') {
+      return 'align-left'
+    }
+  }
+
   constructor(props) {
     super(props);
-    this.state={editorState: EditorState.createEmpty() };
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
     this.onChange = (editorState) => this.setState({editorState})
   }
 
@@ -27,9 +41,9 @@ export default class Documents extends React.Component {
   }
 
   toggleBlockType(event, blockType) {
-    event.preventDefault();
     this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType))
   }
+
 
   render() {
     return (
@@ -39,13 +53,13 @@ export default class Documents extends React.Component {
       <div className="editor">
 
         <div className="toolbar">
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'BOLD')}>B</button>
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'ITALIC')}>I</button>
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'UNDERLINE')}>U</button>
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'STRIKETHROUGH')}>S</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'BOLD')}>Bold</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'ITALIC')}>Italic</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'UNDERLINE')}>Underline</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'STRIKETHROUGH')}>Strikethrough</button>
 
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'UPPERCASE')}>ABC</button>
-            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'LOWERCASE')}>xyz</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'UPPERCASE')}>Uppercase</button>
+            <button onMouseDown={(e) => this.toggleInlineStyle(e, 'LOWERCASE')}>Lowercase</button>
 
             <button onMouseDown={(e) => this.toggleBlockType(e, 'unordered-list-item')}>Unordered List</button>
             <button onMouseDown={(e) => this.toggleBlockType(e, 'ordered-list-item')}>Ordered List</button>
@@ -57,9 +71,23 @@ export default class Documents extends React.Component {
             <button onMouseDown={(e) => this.toggleBlockType(e, 'header-five')}>H5</button>
             <button onMouseDown={(e) => this.toggleBlockType(e, 'header-six')}>H6</button>
 
+            <button onMouseDown={() => this.toggleBlockType(this.state.editorState, 'left')}>Left</button>
+            <button onMouseDown={() => this.toggleBlockType(this.state.editorState, 'center')}>Center</button>
+            <button onMouseDown={() => this.toggleBlockType(this.state.editorState, 'right')}>Right</button>
+
+            <button onMouseDown={()=>this.setState({editorState: EditorState.undo(this.state.editorState)})}>Undo</button>
+            <button onMouseDown={()=>this.setState({editorState: EditorState.redo(this.state.editorState)})}>Redo</button>
+
+
+
 
         </div>
-        <Editor editorState={this.state.editorState} customStyleMap={styleMap} onChange={this.onChange} />
+        <Editor
+          editorState={this.state.editorState}
+          customStyleMap={styleMap}
+          onChange={this.onChange}
+          blockStyleFn={this.myBlockStyleFn}
+        />
 
       </div>
 
