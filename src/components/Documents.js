be1647b1d6dd7dac8e643cbcbd22efe1e471992f
Jan-Philipp <japhib@outlook.com>
2018-07-18 10:28:29 -0700

    edits

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Editor, EditorState, RichUtils, convertFromRaw} from 'draft-js';
+import ColorPicker, { colorPickerPlugin } from 'draft-js-color-picker';
+import createStyles from 'draft-js-custom-styles';
+
+
 
 const styleMap = {
   'UPPERCASE': {
@@ -10,6 +14,25 @@ const styleMap = {
   }
 }
 
+const presetColors = [
+  '#ff00aa',
+  '#F5A623',
+  '#F8E71C',
+  '#8B572A',
+  '#7ED321',
+  '#417505',
+  '#BD10E0',
+  '#9013FE',
+  '#4A90E2',
+  '#50E3C2',
+  '#B8E986',
+  '#000000',
+  '#4A4A4A',
+  '#9B9B9B',
+  '#FFFFFF',
+];
+
+const {styles,customStyleFn } = createStyles(['font-size'])
 
 export default class Documents extends React.Component {
 
@@ -32,27 +55,46 @@ export default class Documents extends React.Component {
       editorState: EditorState.createEmpty(),
     };
     this.onChange = (editorState) => this.setState({editorState})
+    this.getEditorState = () => this.state.editorState;
+    this.picker = colorPickerPlugin(this.onChange, this.getEditorState);
+    this.updateEditorState = editorState => this.setState({ editorState });
+    this.toggleFontSize = this.toggleFontSize.bind(this);
   }
 
+  onChange(editorState) {
+    this.setState({editorState});
+  }
+
+  toggleFontSize(fontSize) {
+    console.log(fontSize);
+    const newEditorState = styles.fontSize.toggle(this.state.editorState, fontSize);
+    console.log(newEditorState);
+    return this.updateEditorState(newEditorState);
+  }
 
   toggleInlineStyle(event,inlineStyle) {
     event.preventDefault();
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle))
-  }
+  };
 
   toggleBlockType(event, blockType) {
     this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType))
-  }
+  };
 
 
   render() {
+
+    const options = x => x.map(fontSize => {
+          return <option key={fontSize} value={fontSize}>{fontSize}</option>;
+        });
+
     return (
       <div id="content">
         <h1>Document Editor </h1>
 
-      <div className="editor">
+        <div className="editor">
 
-        <div className="toolbar">
+          <div className="toolbar">
             <button onMouseDown={(e) => this.toggleInlineStyle(e, 'BOLD')}>Bold</button>
             <button onMouseDown={(e) => this.toggleInlineStyle(e, 'ITALIC')}>Italic</button>
             <button onMouseDown={(e) => this.toggleInlineStyle(e, 'UNDERLINE')}>Underline</button>
@@ -78,18 +120,27 @@ export default class Documents extends React.Component {
             <button onMouseDown={()=>this.setState({editorState: EditorState.undo(this.state.editorState)})}>Undo</button>
             <button onMouseDown={()=>this.setState({editorState: EditorState.redo(this.state.editorState)})}>Redo</button>
 
+            <select onChange={e => this.toggleFontSize(e.target.value)}>
+            {options(['6px', '8px', '10px', '12px', '14px', '16px', '18px', '20px', '22px', '24px', '26px', '28px', '30px', '32px', '34px', '36px', '50px', '72px'])}
+            </select>
 
+          </div>
 
+          <ColorPicker
+            toggleColor={color => this.picker.addColor(color)}
+            presetColors={presetColors}
+            color={this.picker.currentColor(this.state.editorState)}
+          />
+
+          <Editor
+            editorState={this.state.editorState}
+            customStyleMap={styleMap}
+            onChange={(e) => this.onChange(e)}
+            blockStyleFn={this.myBlockStyleFn}
+            customStyleFn={this.picker.customStyleFn}
+          />
 
         </div>
-        <Editor
-          editorState={this.state.editorState}
-          customStyleMap={styleMap}
-          onChange={this.onChange}
-          blockStyleFn={this.myBlockStyleFn}
-        />
-
-      </div>
 
       </div>
     );
